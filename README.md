f906e6f254f94a18c71c1f468405eb838ded9912
Julian McCarty <Julianmccarty32@gmail.com>
2024-08-05 21:46:49 -0300

    Added transaction history

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BankingSystem
+# Project concepts taken from Tech With Tim https://www.youtube.com/watch?v=Dn1EjhcQk64
 Python project, showcasing concepts like: pandas, user input, using a CSV file to store information,  
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from data import getAmount, getCategory, getDate, getDescription
 class CSV():
     CSVFile = 'Transaction_Data.csv'
     columns = ["date", "amount", "category", "description"]
-    
+    dateFormat =  "%d-%m-%Y"
+
     @classmethod
     def initialize_csv(cls):
         try: 
@@ -30,8 +31,37 @@ class CSV():
             writer.writerow(newData)
             
         print("Added Entry, What else can we help you with today?")
+    
+    @classmethod
+    def getTransactions(cls, startDate, endDate):
+        df = pd.read_csv(cls.CSVFile)
         
-
+        #Convert all dates into date time object to filter transactions
+        df["date"] = pd.to_datetime(df["date"], format=CSV.dateFormat)
+        startDate = datetime.strptime(startDate, CSV.dateFormat)
+        endDate = datetime.strptime(endDate, CSV.dateFormat)
+        
+        #Create a Mask to see if we should select rows from transaction data 
+        mask = (df["date"] >= startDate ) & (df["date"] <= endDate)
+        
+        #Creates a df where only items between the start and end date are in it
+        filteredDf = df.loc[mask]
+        
+        if filteredDf.empty:
+            print("No transactions found in the given date range")
+        else:
+            print(f"Transactions from {startDate.strftime(CSV.dateFormat)} to {endDate.strftime(CSV.dateFormat)}")
+            print(filteredDf.to_string(index=False, formatters={"date": lambda x: x.strftime(CSV.dateFormat)}))
+            
+            totalIncome = filteredDf[filteredDf["category"] == "Income"]["amount"].sum()
+            totalExpense = filteredDf[filteredDf["category"] == "Expense"]["amount"].sum()
+            print("\nSummary: ")
+            print(f"Total Income: ${totalIncome:.2f}")
+            print(f"Total Expense: ${totalExpense:.2f}")
+            print(f"Total Savings: ${(totalIncome - totalExpense):.2f}")
+        
+        return filteredDf
+            
 def add():
     CSV.initialize_csv()
     date = getDate("Enter the date of the Transaction (dd-mm-yyyy) or press enter for todays date: ", allowDefault=True)
@@ -45,7 +75,7 @@ def add():
 def main():
     while True:
         print('\n1. Add a new transaction')
-        print('2. View previous transactions')
+        print('2. get previous transactions')
         print('3. Exit')
         request = input('Select your request by entering (1-3): ')
         
@@ -57,7 +87,8 @@ def main():
             add()
         
         if request == '2':
-            pass
+            
+            df = CSV.getTransactions("05-03-2023", "05-08-2024")
 
     
 if __name__ == '__main__':
